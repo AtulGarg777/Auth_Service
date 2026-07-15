6a3a5acb586572a75148c3cb8a7ebf7e3adb023f
Atul Garg <agarg7731@gmail.com>
2026-07-15 19:03:07 +0530

    changes in login pages of backend and frontend

--- backend/controllers/Signup.js
+++ backend/controllers/Signup.js
@@ -1,12 +1,12 @@
 const UserModel = require("../models/User");
-const jwt=require('jsonwebtoken');
-const bcrypt=require('bcrypt')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
 
 const signup = async (req, res) => {
     try {
         let { name, email, password } = req.body;
         let user = await UserModel.findOne({ email });
-        
+
         if (user) {
             return res.status(409).json({ message: 'user already exist,go for login', success: false })
         }
@@ -16,9 +16,7 @@ const signup = async (req, res) => {
         res.status(201).json({ message: 'signup successfully', success: true });
 
     } catch (error) {
-        console.log(error);
-        
-        res.status(500).json({ message: 'Internal server error',error, success: false })
+        res.status(500).json({ message: 'Internal server error', error, success: false})
     }
 }
 
@@ -27,7 +25,7 @@ const login = async (req, res) => {
         let { email, password } = req.body;
         let user = await UserModel.findOne({ email });
         if (!user) {
-           return res.status(403).json({ message: 'Email or Password is wrong', success: false })
+            return res.status(403).json({ message: 'Email or Password is wrong', success: false })
         }
         let checkPass = await bcrypt.compare(password, user.password);
         if (!checkPass) {
@@ -43,7 +41,7 @@ const login = async (req, res) => {
         res.status(200).json({ message: 'login successfully', success: true, jwtToken, name: user.name, email })
 
     } catch (error) {
-        res.status(500).json({ message: 'Internal server error', success: false })
+        res.status(500).json({ message: 'Internal server error', success: false, error })
     }
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -44,6 +44,8 @@ function Login() {
                 handleError(message);
             }
         } catch (error) {
+            console.log("catch block");
+            
             handleError(error);
         }
     }
